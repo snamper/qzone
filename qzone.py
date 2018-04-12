d3883c7a7531ed5835a379323027f70a43f3d41c
pjatgithub <pjatgithub@users.noreply.github.com>
2018-04-13 07:15:56 +0800

    Fix bugs in visiting qzone

--- qzone.py
+++ qzone.py
@@ -1,50 +1,83 @@
 from selenium import webdriver
+from selenium.common.exceptions import WebDriverException
 import data
 import json
+import time
+import random
 
-def get_browser.):
+def get_browser_driver():
     # Get the proper browser.path
     import platform
 
     os_name = platform.system()
 
     if os_name == 'Linux':
-        browser.path = 'linux/chromedriver'
+        driver_path = 'linux/chromedriver'
     elif os_name == 'Darwin':
-        browser.path = 'mac/chromedriver'
+        driver_path = 'mac/chromedriver'
     elif os_name == 'Windows':
-        browser.path = 'win/chromedriver.exe'
+        driver_path = 'win/chromedriver.exe'
     else:
         raise NotImplementedError("Not supported on this operating system!")
 
-    return 'browser.chrome/{}'.format(driver_path)
+    return 'driver/chrome/{}'.format(driver_path)
 
 def create_browser():
-    return selenium.Chrome(get_browser.
+    return webdriver.Chrome(get_browser_driver())
 
 def visit():
     browser = create_browser()
     ants = get_ants()
     bosses = get_bosses()
     login_url = 'https://i.qq.com'
-    boss_url = 'https://user.qzone.com/{}'
+    boss_url = 'https://user.qzone.qq.com/{}'
 
-    for ant in ants:
-        browser.get(login_url)
-        browser.switch_to.frame('login_frame')
-        browser.find_element_by_id('switcher_plogin').click()
-        browser.find_element_by_id('u').clear()
-        browser.find_element_by_id('u').send_keys(ant.__name__)
-        browser.find_element_by_id('p').clear()
-        browser.find_element_by_id('p').send_keys(ant.__password__)
-        browser.find_element_by_id('login_button').click()
+    try:
+        for ant in ants:
+            browser.get(login_url)
+            browser.switch_to.frame('login_frame')
+            browser.find_element_by_id('switcher_plogin').click()
+            browser.find_element_by_id('u').clear()
+            browser.find_element_by_id('u').send_keys(ant.__name__)
+            browser.find_element_by_id('p').clear()
+            browser.find_element_by_id('p').send_keys(ant.__password__)
+            browser.find_element_by_id('login_button').click()
 
-        for boss in bosses:
-            browser.get(boss_url.format(bosses.__name__))
+            skip = False
 
-        browser.find_element_by_id('tb_logout').click()
-        alert = browser.switch_to_alert()
-        alert.accept()
+            for boss in bosses:
+                browser.get(boss_url.format(boss.__name__))
+
+                # check if log in successfully
+                try:
+                    browser.find_element_by_id('QM_OwnerInfo_Icon')
+                except WebDriverException:
+                    skip = True
+                    break
+
+                try:
+                    browser.find_element_by_class_name('btn-fs-sure').click()
+                except WebDriverException:
+                    pass
+
+                print(ant.__name__, 'has visited', boss.__name__)
+
+            if skip:
+                print(ant.__name__, 'may have wrong password', ant.__password__)
+                continue
+
+            time.sleep(random.randrange(1,4))
+            browser.find_element_by_id('tb_logout').click()
+            alert = browser.switch_to_alert()
+            alert.accept()
+            time.sleep(random.randrange(1,4))
+
+    except WebDriverException as e:
+        print(e)
+        print(ant, boss)
+        browser.quit()
+    else:
+        browser.quit()
 
 def check_config(path):
     from pathlib import Path
@@ -59,7 +92,7 @@ def get_ants(path = 'config/ants.json'):
 
     return ants
 
-def get_bosses(path = 'config/boss.json'):
+def get_bosses(path = 'config/bosses.json'):
     check_config(path)
 
     with open(path, 'r', encoding = 'utf-8') as bosses_config:
