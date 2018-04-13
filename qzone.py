21e27f8b0e6257f7e057d194ca914de72dca2976
pjatgithub <pjatgithub@users.noreply.github.com>
2018-04-13 10:22:02 +0800

    Improve the visiting process

--- qzone.py
+++ qzone.py
@@ -4,9 +4,10 @@ import data
 import json
 import time
 import random
+import logging, logging.config
 
 def get_browser_driver():
-    # Get the proper browser.path
+    '''Get the proper browser.path'''
     import platform
 
     os_name = platform.system()
@@ -23,68 +24,128 @@ def get_browser_driver():
     return 'driver/chrome/{}'.format(driver_path)
 
 def create_browser():
+    '''Get a new browser'''
     return webdriver.Chrome(get_browser_driver())
 
 def visit():
     browser = create_browser()
+    logger.info('Browser created successfully.')
+
     ants = get_ants()
+    logger.info('{} ants loaded.'.format(len(ants)))
+
     bosses = get_bosses()
+    logger.info('{} bosses loaded'.format(len(bosses)))
+
     login_url = 'https://i.qq.com'
-    boss_url = 'https://user.qzone.qq.com/{}'
+    boss_url_formatter = 'https://user.qzone.qq.com/{}'
 
-    try:
-        for ant in ants:
-            browser.get(login_url)
-            browser.switch_to.frame('login_frame')
-            browser.find_element_by_id('switcher_plogin').click()
-            browser.find_element_by_id('u').clear()
-            browser.find_element_by_id('u').send_keys(ant.__name__)
-            browser.find_element_by_id('p').clear()
-            browser.find_element_by_id('p').send_keys(ant.__password__)
-            browser.find_element_by_id('login_button').click()
+    for ant in ants:
+        login(browser, ant, login_url)
+        sleep_random()
 
-            skip = False
+        for boss in bosses:
+            boss_url = boss_url_formatter.format(boss.__name__)
+            visit_boss(browser, ant, boss, boss_url)
 
-            for boss in bosses:
-                browser.get(boss_url.format(boss.__name__))
+        logout(browser, ant, login_url)
 
-                # check if log in successfully
-                try:
-                    browser.find_element_by_id('QM_OwnerInfo_Icon')
-                except WebDriverException:
-                    skip = True
-                    break
+    browser.quit()
 
-                try:
-                    browser.find_element_by_class_name('btn-fs-sure').click()
-                except WebDriverException:
-                    pass
+def visit_boss(browser, ant, boss, boss_url):
+    '''Visit boss'''
+    visited, counter, max_times= False, 0, 10
 
-                print(ant.__name__, 'has visited', boss.__name__)
+    while not visited and counter < max_times:
+        browser.get(boss_url)
+        sleep_random(1000, 1500)
 
-            if skip:
-                print(ant.__name__, 'may have wrong password', ant.__password__)
-                continue
+        visited = has_visited(browser)
+        counter += 1
 
-            time.sleep(random.randrange(1,4))
-            browser.find_element_by_id('tb_logout').click()
-            alert = browser.switch_to_alert()
-            alert.accept()
-            time.sleep(random.randrange(1,4))
-
-    except WebDriverException as e:
-        print(e)
-        print(ant, boss)
-        browser.quit()
+    if visited:
+        logger.info('{0} has visited {1} successfully.'.format(ant.__name__, boss.__name__))
     else:
-        browser.quit()
+        logger.warn('{0} can not visit {1} after \
+            trying {2} times.'.format(ant.__name__, boss.__name__, max_times))
+
+def login(browser, ant, login_url):
+    '''Log in Qzone'''
+    import vlc
+
+    name, password = ant.__name__, ant.__password__
+    browser.get(login_url)
+    browser.switch_to.frame('login_frame')
+    browser.find_element_by_id('switcher_plogin').click()
+    browser.find_element_by_id('u').clear()
+    browser.find_element_by_id('u').send_keys(name)
+    browser.find_element_by_id('p').clear()
+    browser.find_element_by_id('p').send_keys(password)
+    browser.find_element_by_id('login_button').click()
+    sleep_random()
+
+    while not has_logged_in(browser):
+        logger.warn('{} needs to be verified!'.format(name))
+        notify_path = 'voice/notify_verify.mp3'
+        player = vlc.MediaPlayer(notify_path)
+        player.play()
+        time.sleep(15)
+        player.release()
+
+    close_help_page(browser)
+
+    logger.info('{} has logged in successfully.'.format(name))
+
+def close_help_page(browser):
+    '''Close the help page if existing'''
+    try:
+        browser.find_element_by_class_name('btn-fs-sure').click()
+    except WebDriverException:
+        pass
+
+def has_visited(browser):
+    '''Check if visiting'''
+    try:
+        browser.find_element_by_id('QM_OwnerInfo_Icon')
+        return True
+    except WebDriverException:
+        return False
+
+def has_logged_in(browser):
+    '''Check if log in'''
+    sleep_random(500, 1000)
+    try:
+        browser.find_element_by_id('newVcodeArea')
+        return False
+    except WebDriverException:
+        return True
+
+def logout(browser, ant, logout_url):
+    '''Log out Qzone'''
+    while True:
+        browser.get(logout_url)
+
+        if has_logged_in(browser):
+            try:
+                browser.find_element_by_id('tb_logout').click()
+                alert = browser.switch_to_alert()
+                alert.accept()
+                break
+            except WebDriverException:
+                pass
+        else:
+            break
+
+    logger.info('{} has logged out successfully.'.format(ant.__name__))
 
 def check_config(path):
+    '''Check if the config file exists'''
     from pathlib import Path
 
     assert Path(path).exists(), path + ' does not exist!'
 
 def get_ants(path = 'config/ants.json'):
+    '''Get working ants from json'''
     check_config(path)
 
     with open(path, 'r', encoding = 'utf-8') as ants_config:
@@ -93,12 +154,21 @@ def get_ants(path = 'config/ants.json'):
     return ants
 
 def get_bosses(path = 'config/bosses.json'):
+    '''Get bosses from json'''
     check_config(path)
 
     with open(path, 'r', encoding = 'utf-8') as bosses_config:
         bosses = json.load(bosses_config, cls = data.QQDecoder)
 
     return bosses
+
+def sleep_random(lower = 200, upper = 500):
+    '''Sleep randomly from 200 ms to 500 ms'''
+    time.sleep(random.randrange(lower, upper) / 1000)
+
+# Logger
+logging.config.fileConfig('config/logging.config')
+logger = logging.getLogger('simpleExample')
 
 if __name__ == '__main__':
     visit()
